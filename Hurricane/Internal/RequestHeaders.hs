eaef114ad2d960ddf8b5a21728a795c3d934d1d7
Ankur Goyal <ankur@agmbp.local>
2011-05-29 21:05:40 -0400

    Added raw text for each of the header modules. Going to use it for static handler

--- Hurricane/Internal/RequestHeaders.hs
+++ Hurricane/Internal/RequestHeaders.hs
@@ -1,45 +1,164 @@
+{-# Language OverloadedStrings #-}
+
 module Hurricane.Internal.RequestHeaders
 (
-  RequestHeaders(..)
+  RequestHeaders(..),
+  acceptHeaderName,
+  acceptCharsetHeaderName,
+  acceptEncodingHeaderName,
+  acceptLanguageHeaderName,
+  authorizationHeaderName,
+  cachecontrolHeaderName,
+  connectionHeaderName,
+  cookieHeaderName,
+  contentLengthHeaderName,
+  contentMD5HeaderName,
+  contentTypeHeaderName,
+  dateHeaderName,
+  expectHeaderName,
+  fromHeaderName,
+  hostHeaderName,
+  ifMatchHeaderName,
+  ifModifiedSinceHeaderName,
+  ifNoneMatchHeaderName,
+  ifRangeHeaderName,
+  ifUnmodifiedSinceHeaderName,
+  maxForwardsHeaderName,
+  pragmaHeaderName,
+  proxyAuthorizationHeaderName,
+  rangeHeaderName,
+  refererHeaderName,
+  tEHeaderName,
+  upgradeHeaderName,
+  userAgentHeaderName,
+  viaHeaderName,
+  warningHeaderName
 ) where
 
-import qualified Data.ByteString as B
+import qualified Data.Text as T
 import qualified Network.HTTP.Types as H
-
 
 {-- Headers that we don't actively used are just
  -- ByteStrings for now --}
 data RequestHeaders = RequestHeaders {
-  accept :: Maybe B.ByteString,
-  acceptCharset :: Maybe B.ByteString,
-  acceptEncoding :: Maybe B.ByteString,
-  acceptLanguage :: Maybe B.ByteString,
-  authorization :: Maybe B.ByteString,
-  cachecontrol :: Maybe B.ByteString,
-  connection :: Maybe B.ByteString,
-  cookie :: Maybe B.ByteString,
-  contentLength :: Maybe B.ByteString,
-  contentMD5 :: Maybe B.ByteString,
-  contentType :: Maybe B.ByteString,
-  date :: Maybe B.ByteString,
-  expect :: Maybe B.ByteString,
-  from :: Maybe B.ByteString,
-  host :: Maybe B.ByteString,
-  ifMatch :: Maybe B.ByteString,
-  ifModifiedSince :: Maybe B.ByteString,
-  ifNoneMatch :: Maybe B.ByteString,
-  ifRange :: Maybe B.ByteString,
-  ifUnmodifiedSince :: Maybe B.ByteString,
-  maxForwards :: Maybe B.ByteString,
-  pragma :: Maybe B.ByteString,
-  proxyAuthorization :: Maybe B.ByteString,
-  range :: Maybe B.ByteString,
-  referer :: Maybe B.ByteString,
-  tE :: Maybe B.ByteString,
-  upgrade :: Maybe B.ByteString,
-  userAgent :: Maybe B.ByteString,
-  via :: Maybe B.ByteString,
-  warning :: Maybe B.ByteString
+  accept :: Maybe T.Text,
+  acceptCharset :: Maybe T.Text,
+  acceptEncoding :: Maybe T.Text,
+  acceptLanguage :: Maybe T.Text,
+  authorization :: Maybe T.Text,
+  cachecontrol :: Maybe T.Text,
+  connection :: Maybe T.Text,
+  cookie :: Maybe T.Text,
+  contentLength :: Maybe T.Text,
+  contentMD5 :: Maybe T.Text,
+  contentType :: Maybe T.Text,
+  date :: Maybe T.Text,
+  expect :: Maybe T.Text,
+  from :: Maybe T.Text,
+  host :: Maybe T.Text,
+  ifMatch :: Maybe T.Text,
+  ifModifiedSince :: Maybe T.Text,
+  ifNoneMatch :: Maybe T.Text,
+  ifRange :: Maybe T.Text,
+  ifUnmodifiedSince :: Maybe T.Text,
+  maxForwards :: Maybe T.Text,
+  pragma :: Maybe T.Text,
+  proxyAuthorization :: Maybe T.Text,
+  range :: Maybe T.Text,
+  referer :: Maybe T.Text,
+  tE :: Maybe T.Text,
+  upgrade :: Maybe T.Text,
+  userAgent :: Maybe T.Text,
+  via :: Maybe T.Text,
+  warning :: Maybe T.Text
 }
 
-parseHeaders :: H.RequestHeaders
+acceptHeaderName :: T.Text
+acceptHeaderName = "Accept"
+
+acceptCharsetHeaderName :: T.Text
+acceptCharsetHeaderName = "Accept-Charset"
+
+acceptEncodingHeaderName :: T.Text
+acceptEncodingHeaderName = "Accept-Encoding"
+
+acceptLanguageHeaderName :: T.Text
+acceptLanguageHeaderName = "Accept-Language"
+
+authorizationHeaderName :: T.Text
+authorizationHeaderName = "Authorization"
+
+cachecontrolHeaderName :: T.Text
+cachecontrolHeaderName = "Cache-Control"
+
+connectionHeaderName :: T.Text
+connectionHeaderName = "Connection"
+
+cookieHeaderName :: T.Text
+cookieHeaderName = "Cookie"
+
+contentLengthHeaderName :: T.Text
+contentLengthHeaderName = "Content-Length"
+
+contentMD5HeaderName :: T.Text
+contentMD5HeaderName = "Content-MD5"
+
+contentTypeHeaderName :: T.Text
+contentTypeHeaderName = "Content-Type"
+
+dateHeaderName :: T.Text
+dateHeaderName = "Date"
+
+expectHeaderName :: T.Text
+expectHeaderName = "Expect"
+
+fromHeaderName :: T.Text
+fromHeaderName = "From"
+
+hostHeaderName :: T.Text
+hostHeaderName = "Host"
+
+ifMatchHeaderName :: T.Text
+ifMatchHeaderName = "If-Match"
+
+ifModifiedSinceHeaderName :: T.Text
+ifModifiedSinceHeaderName = "If-Modified-Since"
+
+ifNoneMatchHeaderName :: T.Text
+ifNoneMatchHeaderName = "If-None-Match"
+
+ifRangeHeaderName :: T.Text
+ifRangeHeaderName = "If-Range"
+
+ifUnmodifiedSinceHeaderName :: T.Text
+ifUnmodifiedSinceHeaderName = "If-Unmodified-Since"
+
+maxForwardsHeaderName :: T.Text
+maxForwardsHeaderName = "Max-Forwards"
+
+pragmaHeaderName :: T.Text
+pragmaHeaderName = "Pragma"
+
+proxyAuthorizationHeaderName :: T.Text
+proxyAuthorizationHeaderName = "Proxy-Authorization"
+
+rangeHeaderName :: T.Text
+rangeHeaderName = "Range"
+
+refererHeaderName :: T.Text
+refererHeaderName = "Referer"
+
+tEHeaderName :: T.Text
+tEHeaderName = "TE"
+
+upgradeHeaderName :: T.Text
+upgradeHeaderName = "Upgrade"
+
+userAgentHeaderName :: T.Text
+userAgentHeaderName = "User-Agent"
+
+viaHeaderName :: T.Text
+viaHeaderName = "Via"
+
+warningHeaderName :: T.Text
+warningHeaderName = "Warning"
--- Hurricane/Internal/ResponseHeaders.hs
+++ Hurricane/Internal/ResponseHeaders.hs
@@ -1,41 +1,171 @@
+{-# Language OverloadedStrings #-}
+
 module Hurricane.Internal.ResponseHeaders
 (
-  ResponseHeaders(..)
+  ResponseHeaders(..),
+  acceptRangesHeaderName,
+  ageHeaderName,
+  allowHeaderName,
+  cacheControlHeaderName,
+  connectionHeaderName,
+  contentEncodingHeaderName,
+  contentLanguageHeaderName,
+  contentLengthHeaderName,
+  contentLocationHeaderName,
+  contentMD5HeaderName,
+  contentDispositionHeaderName,
+  contentRangeHeaderName,
+  contentTypeHeaderName,
+  dateHeaderName,
+  eTagHeaderName,
+  expiresHeaderName,
+  lastModifiedHeaderName,
+  linkHeaderName,
+  locationHeaderName,
+  p3PHeaderName,
+  pragmaHeaderName,
+  proxyAuthenticateHeaderName,
+  refreshHeaderName,
+  retryAfterHeaderName,
+  serverHeaderName,
+  setCookieHeaderName,
+  trailerHeaderName,
+  transferEncodingHeaderName,
+  varyHeaderName,
+  viaHeaderName,
+  warningHeaderName,
+  wwwAuthenticateHeaderName
 ) where
 
-import qualified Data.ByteString as B
+import qualified Data.Text as T
 
 data ResponseHeaders = ResponseHeaders {
-  acceptRanges :: Maybe B.ByteString,
-  age :: Maybe B.ByteString,
-  allow :: Maybe B.ByteString,
-  cacheControl :: Maybe B.ByteString,
-  connection :: Maybe B.ByteString,
-  contentEncoding :: Maybe B.ByteString,
-  contentLanguage :: Maybe B.ByteString,
-  contentLength :: Maybe B.ByteString,
-  contentLocation :: Maybe B.ByteString,
-  contentMD5 :: Maybe B.ByteString,
-  contentDisposition :: Maybe B.ByteString,
-  contentRange :: Maybe B.ByteString,
-  contentType :: Maybe B.ByteString,
-  date :: Maybe B.ByteString,
-  eTag :: Maybe B.ByteString,
-  expires :: Maybe B.ByteString,
-  lastModified :: Maybe B.ByteString,
-  link :: Maybe B.ByteString,
-  location :: Maybe B.ByteString,
-  p3P :: Maybe B.ByteString,
-  pragma :: Maybe B.ByteString,
-  proxyAuthenticate :: Maybe B.ByteString,
-  refresh :: Maybe B.ByteString,
-  retryAfter :: Maybe B.ByteString,
-  server :: Maybe B.ByteString,
-  setCookie :: Maybe B.ByteString,
-  trailer :: Maybe B.ByteString,
-  transferEncoding :: Maybe B.ByteString,
-  vary :: Maybe B.ByteString,
-  via :: Maybe B.ByteString,
-  warning :: Maybe B.ByteString,
-  wwwAuthenticate :: Maybe B.ByteString
+  acceptRanges :: Maybe T.Text,
+  age :: Maybe T.Text,
+  allow :: Maybe T.Text,
+  cacheControl :: Maybe T.Text,
+  connection :: Maybe T.Text,
+  contentEncoding :: Maybe T.Text,
+  contentLanguage :: Maybe T.Text,
+  contentLength :: Maybe T.Text,
+  contentLocation :: Maybe T.Text,
+  contentMD5 :: Maybe T.Text,
+  contentDisposition :: Maybe T.Text,
+  contentRange :: Maybe T.Text,
+  contentType :: Maybe T.Text,
+  date :: Maybe T.Text,
+  eTag :: Maybe T.Text,
+  expires :: Maybe T.Text,
+  lastModified :: Maybe T.Text,
+  link :: Maybe T.Text,
+  location :: Maybe T.Text,
+  p3P :: Maybe T.Text,
+  pragma :: Maybe T.Text,
+  proxyAuthenticate :: Maybe T.Text,
+  refresh :: Maybe T.Text,
+  retryAfter :: Maybe T.Text,
+  server :: Maybe T.Text,
+  setCookie :: Maybe T.Text,
+  trailer :: Maybe T.Text,
+  transferEncoding :: Maybe T.Text,
+  vary :: Maybe T.Text,
+  via :: Maybe T.Text,
+  warning :: Maybe T.Text,
+  wwwAuthenticate :: Maybe T.Text
 }
+
+acceptRangesHeaderName :: T.Text
+acceptRangesHeaderName = "Accept-Ranges"
+
+ageHeaderName :: T.Text
+ageHeaderName = "Age"
+
+allowHeaderName :: T.Text
+allowHeaderName = "Allow"
+
+cacheControlHeaderName :: T.Text
+cacheControlHeaderName = "Cache-Control"
+
+connectionHeaderName :: T.Text
+connectionHeaderName = "Connection"
+
+contentEncodingHeaderName :: T.Text
+contentEncodingHeaderName = "Content-Encoding"
+
+contentLanguageHeaderName :: T.Text
+contentLanguageHeaderName = "Content-Language"
+
+contentLengthHeaderName :: T.Text
+contentLengthHeaderName = "Content-Length"
+
+contentLocationHeaderName :: T.Text
+contentLocationHeaderName = "Content-Location"
+
+contentMD5HeaderName :: T.Text
+contentMD5HeaderName = "Content-MD5"
+
+contentDispositionHeaderName :: T.Text
+contentDispositionHeaderName = "Content-Disposition"
+
+contentRangeHeaderName :: T.Text
+contentRangeHeaderName = "Content-Range"
+
+contentTypeHeaderName :: T.Text
+contentTypeHeaderName = "Content-Type"
+
+dateHeaderName :: T.Text
+dateHeaderName = "Date"
+
+eTagHeaderName :: T.Text
+eTagHeaderName = "ETag"
+
+expiresHeaderName :: T.Text
+expiresHeaderName = "Expires"
+
+lastModifiedHeaderName :: T.Text
+lastModifiedHeaderName = "Last-Modified"
+
+linkHeaderName :: T.Text
+linkHeaderName = "Link"
+
+locationHeaderName :: T.Text
+locationHeaderName = "Location"
+
+p3PHeaderName :: T.Text
+p3PHeaderName = "P3P"
+
+pragmaHeaderName :: T.Text
+pragmaHeaderName = "Pragma"
+
+proxyAuthenticateHeaderName :: T.Text
+proxyAuthenticateHeaderName = "Proxy-Authenticate"
+
+refreshHeaderName :: T.Text
+refreshHeaderName = "Refresh"
+
+retryAfterHeaderName :: T.Text
+retryAfterHeaderName = "Retry-After"
+
+serverHeaderName :: T.Text
+serverHeaderName = "Server"
+
+setCookieHeaderName :: T.Text
+setCookieHeaderName = "Set-Cookie"
+
+trailerHeaderName :: T.Text
+trailerHeaderName = "Trailer"
+
+transferEncodingHeaderName :: T.Text
+transferEncodingHeaderName = "Transfer-Encoding"
+
+varyHeaderName :: T.Text
+varyHeaderName = "Vary"
+
+viaHeaderName :: T.Text
+viaHeaderName = "Via"
+
+warningHeaderName :: T.Text
+warningHeaderName = "Warning"
+
+wwwAuthenticateHeaderName :: T.Text
+wwwAuthenticateHeaderName = "WWW-Authenticate"
